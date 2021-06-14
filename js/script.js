9167ce5ba85d1f85e78e9216d64d3d4cfe6d9839
Marco Tessitore <marcotexcode@gmail.com>
2021-06-14 14:24:27 +0200

    esercizio step5

--- js/script.js
+++ js/script.js
@@ -23,6 +23,68 @@ var studente = {
 //STAMPARE A SCHERMO TUTTE LE PROPRIETA
 
 for (var k in studente) {
-    console.log(k + ': ' + studente[k]);
+
+    //console.log(k + ': ' + studente[k]);
+
 }
 
+
+//CREARE UN ARRAY DI OGGETTI DI STUDENTI
+
+var studenti = [
+    {
+        nome: 'Marco',
+        cognome: 'Tessitore',
+        eta: '31',
+    },
+
+    {
+        nome: 'Giovanni',
+        cognome: 'Rossi',
+        eta: '21',
+    },
+
+    {
+        nome: 'Aldo',
+        cognome: 'Neri',
+        eta: '45',
+    }
+
+];
+
+
+
+
+
+
+// CICLARE TUTTI GLI STUDENTI E STAMPARE PER OGIUNO DI ESSI NOME E COGNOME
+
+for (var i = 0; i < studenti.length; i++) {
+
+   // console.log('Nome: ' + studenti[i].nome);
+   // console.log('Cognome: ' +  studenti[i].cognome);
+
+}
+
+// DARE LA POSSIBILITA ALL UTENTE DI INSERIRE CON TRE PROMPT DI AGGIUNGERE NOME COGNOME ETA DENTRO L'OGGETTO STUDENTE
+
+var nome = prompt('Inserisci il nome: ');
+var cognome = prompt('Inserisci il cognome: ');
+var eta = parseInt(prompt('Inserisci l eta'));
+
+
+var nuovoStudente = {
+    nome: nome,
+    cognome: cognome, 
+    eta: eta
+};
+ 
+
+studenti.push(nuovoStudente);
+
+
+for(var i = 0; i < studenti.length; i++) {
+    for( var k in studenti[i]) {
+       // console.log(k + ': ' + studente[k]);
+    }
+}
